d43dd212bbe8393b8dac8fe3d1dfc9b43266dc7f
tktk <thangkhungtheki@gmail.com>
2024-06-04 11:58:57 +0700

    cors

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(5000,{
+  cors: {
+    origin: ["http://192.168.2.134:3000","http://127.0.0.1:3000"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
 io.on('connection', (socket) => {
-  console.log('New client connected');
+  socket.emit('User connected', socket.id)
+  console.log('New client connected: ',socket.id);
 
   // Xử lý các sự kiện từ client
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 
+  socket.on('chat',(data)=>{
+    console.log('Client id: %s : %s', socket.id, data)
+  })
+
+  
   // Example: Khi database được cập nhật, phát sự kiện tới tất cả các client
 //   function broadcastUpdate(data) {
 //     io.emit('update', data);
